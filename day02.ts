6de340463d57c7bb683466fe1c2f82ecd4023617
Ronny Rook <info@ronnyrook.nl>
2023-12-18 11:49:14 +0100

    Part 2 day 2

--- day02.ts
+++ day02.ts
@@ -8,6 +8,68 @@ const maxRedCubes = 12;
 const maxGreenCubes = 13;
 const maxBlueCubes = 14;
 
+// Part 2
+const mapHighestNumbersPerColor = lines
+  .map(line => {
+    const [gameId, cubes] = line.split(':')
+    const cubesPerRound = cubes.split(';');
+
+    return cubesPerRound
+      .map((round) => {
+          const values = round.split(',');
+
+          let redCubes: number[] = [];
+          let greenCubes: number[] = [];
+          let blueCubes: number[] = [];
+
+          values.forEach((value) => {
+            const amount = Number(value.trim().match(/\d+/));
+
+            if (value.includes('red')) {
+              redCubes.push(amount)
+            } else if (value.includes('green')) {
+              greenCubes.push(amount)
+            } else if (value.includes('blue')) {
+              blueCubes.push(amount)
+            }
+          })
+
+          const maxRedCubes = redCubes.length > 0 ? Math.max(...redCubes) : 0;
+          const maxGreenCubes = greenCubes.length > 0 ? Math.max(...greenCubes) : 0;
+          const maxBlueCubes = blueCubes.length > 0 ? Math.max(...blueCubes) : 0;
+
+          return [maxRedCubes, maxGreenCubes, maxBlueCubes];
+      })
+      .reduce((acc, curr) => {
+        if (curr[0] > acc[0]) {
+          acc[0] = curr[0];
+        }
+
+        if (curr[1] > acc[1]) {
+          acc[1] = curr[1];
+        }
+
+        if (curr[2] > acc[2]) {
+          acc[2] = curr[2];
+        }
+
+        return acc;
+      }, [0,0,0])
+  });
+
+const powerOfNumbers = mapHighestNumbersPerColor.map((value) => {
+  return value[0] * value[1] * value[2];
+})
+
+let sum2 = 0;
+for (let i = 0; i < powerOfNumbers.length; i++ ) {
+  sum2 += powerOfNumbers[i];
+}
+
+console.log({ part2Sum: sum2 });
+
+
+// Part 1
 const validGameNumbers = lines
   .map(line => {
     const [gameId, cubes] = line.split(':')
@@ -48,8 +110,11 @@ const validGameNumbers = lines
   })
   .filter(Boolean) as number[];
 
-let sum = 0;
+let sum1 = 0;
 
 for (let i = 0; i < validGameNumbers.length; i++ ) {
-  sum += validGameNumbers[i];
+  sum1 += validGameNumbers[i];
 }
+
+console.log({ part1Sum: sum1 });
+
