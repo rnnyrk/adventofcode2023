2b8d47d8eea2ac68cc9ff4e5e23cf41bbf3b3c6d
Ronny Rook <info@ronnyrook.nl>
2023-12-19 18:01:08 +0100

    Add comments to day 3

--- day03.ts
+++ day03.ts
@@ -1,4 +1,4 @@
-const path = "./day-3-input.txt";
+const path = "./inputs/day-3-input.txt";
 const file = Bun.file(path);
 
 const text = await file.text();
@@ -13,6 +13,7 @@ function part1() {
     let match;
     let pattern = /\d+/g;
 
+    // Match all numbers and get their start and end positions / index
     while ((match = pattern.exec(lines[lineIndex])) !== null) {
       numbers.push({ 
         start: match.index, 
@@ -21,15 +22,18 @@ function part1() {
       });
     }
 
+    // Loop over all numbers
     for (let number of numbers) {
       let partOfSum = false;
 
+      // For the current line, go 1 line before and 1 line after
       for (let y = lineIndex - 1; y <= lineIndex + 1; y++) {
+        // For all 3 lines (-1, 0 and 1), loop all character in the line
         for (let x = number.start - 1; x <= number.end; x++) {
           if (y >= 0 && y < lines.length && x >= 0 && lines[lineIndex].length > x) {
             const curData = lines[y][x];
-            
 
+            // If the character close to the number are not a number or a dot, it's part of the sum
             if (isNaN(parseInt(curData)) && curData != '.')  {
               partOfSum = true;
             }
@@ -50,7 +54,7 @@ function part2() {
   let total = 0;
   let map = []
 
-  // loop horizontal and vertical lines
+  // all comments are similar, no sum but a map with all numbers and their position
   for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
     let numbers = [];
     let match;
@@ -67,9 +71,7 @@ function part2() {
     for (let number of numbers) {
       for (let y = lineIndex - 1; y <= lineIndex + 1; y++) {
         for (let x = number.start - 1; x <= number.end + 1; x++) {
-          // 
           if (y >= 0 && y < lines.length && x >= 0 && lines[lineIndex].length > x) {
-
             if (lines[y][x] === '*') {
               map.push({ x, y, value: parseInt(number.value) });
             }
