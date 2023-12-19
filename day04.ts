cd9739f5b639971b988953e0b3cae8358897c572
Ronny Rook <info@ronnyrook.nl>
2023-12-19 18:53:23 +0100

    Day 02 part 2

--- day04.ts
+++ day04.ts
@@ -23,6 +23,35 @@ function part1() {
   return result;
 }
 
+function part2() {
+  const cardCount = new Array(lines.length).fill(1);
+
+  lines.forEach((line, index) => {
+    const [, cards] = line.split(": ");
+    const [winning, my] = cards.split(" | ");
+
+    const winningNumbers = winning.match(/\d+/g)?.map((n) => parseInt(n));
+    const myNumbers = my.match(/\d+/g)?.map((n) => parseInt(n));
+
+    if (!winningNumbers || !winningNumbers.length || !myNumbers || !myNumbers.length) return;
+    
+    const points = myNumbers.filter((n) => winningNumbers.includes(n)).length;
+    
+    if (points) {
+      for (let i = index + 1; i < index + 1 + points; i++) {
+        if (cardCount[i]) {
+          cardCount[i] += cardCount[index] || 0
+        }
+      }
+    }
+  });
+
+  // sum of all card counts
+  const result = cardCount.reduce((acc, count) => acc + count, 0);
+  return result;
+}
+
 console.log({ 
   part1: part1(),
+  part2: part2(),
 })
